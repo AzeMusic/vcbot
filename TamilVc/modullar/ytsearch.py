053449fb8ed2f5359e169d5aee52ae32dfb042cb
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-04-17 20:27:59 +0400

    Update and rename TamilVc/modules/ytsearch.py to TamilVc/modullar/ytsearch.py

--- TamilVc/modullar/ytsearch.py
+++ TamilVc/modullar/ytsearch.py
@@ -26,9 +26,9 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
+            text += f"Başlıq - {results[i]['title']}\n"
             text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
+            text += f"Baxışlar - {results[i]['views']}\n"
             text += f"Kanal - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
