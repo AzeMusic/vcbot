0c8e35eb82cc198b86e909f1760c90fbeb9f7522
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-04-17 20:21:18 +0400

    Update private.py

--- TamilVc/modules/private.py
+++ TamilVc/modules/private.py
@@ -25,7 +25,7 @@ def _start(client, message):
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
     await message.reply_text(
-        f"""**🔴 {PROJECT_NAME} is online**""",
+        f"""**🔴 {PROJECT_NAME} onlayındır**""",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💬 Support Chat", url=f"https://t.me/TamilSupport")]]),
     )
 
@@ -62,10 +62,10 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = f"https://t.me/{SUPPORT_GROUP}"
         button = [
-            [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-            [InlineKeyboardButton(text = '📲 Updates', url=f"https://t.me/TamilBots"),
-             InlineKeyboardButton(text = '💬 Support', url=f"https://t.me/TamilSupport")],
-            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}")],
+            [InlineKeyboardButton("✨məni qrupa əlavə et", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+            [InlineKeyboardButton(text = '📲 Kanal', url=f"https://t.me/ledyplaylist"),
+             InlineKeyboardButton(text = '🛠️ Dəstək', url=f"https://t.me/SOQrup")],
+            [InlineKeyboardButton(text = '😈 Owner', url=f"https://t.me/Tenha055")],
             [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
         ]
     else:
