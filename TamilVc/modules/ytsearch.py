e1e25ed475c88a5e339f8689ae777d0855792cad
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-04-17 20:27:01 +0400

    Update ytsearch.py

--- TamilVc/modules/ytsearch.py
+++ TamilVc/modules/ytsearch.py
@@ -21,7 +21,7 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search arqument lazımdır!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Axtarılır...")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
@@ -29,7 +29,7 @@ async def ytsearch(_, message: Message):
             text += f"Title - {results[i]['title']}\n"
             text += f"Duration - {results[i]['duration']}\n"
             text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"Kanal - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
